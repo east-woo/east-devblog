b05e80eecc1770873c9557cd3fe985f8aa92c77e
east-woo <dwseo96@naver.com>
2023-05-11 23:36:25 +0900

    first commit

---
+++ blog-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: "east-woo",
+  description: "Hello :) I'm east-woo.This is a dev blog.",
+  author: "east-woo",
+  siteUrl: "https://east-woo.github.io/gatsby-starter-hoodie/",
+  links: {
+    github: "https://github.com/east-woo",
+    linkedIn: "https://linkedin.com",
+    facebook: "https://www.facebook.com",
+    instagram: "https://www.instagram.com",
+    email: "mailto:dwseo96@wavus.co.kr",
+  },
+  utterances: {
+    repo: "east-woo/gatsby-starter-hoodie",
+    type: "pathname",
+  },
+}
